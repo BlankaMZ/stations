f301e8924f4c6639d4b96b52bc80ee27c1296440
BlankaMZ <blanka.maria.zadzinska@gmail.com>
2024-07-13 16:27:49 +0200

    small UI improvement

--- app/src/main/java/zuri/designs/stationsdistance/screens/HomeScreen.kt
+++ app/src/main/java/zuri/designs/stationsdistance/screens/HomeScreen.kt
@@ -1,18 +1,15 @@
 package zuri.designs.stationsdistance.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -38,41 +34,66 @@ fun HomeScreen(
 ) {
 
     Scaffold(
-        topBar = { TopAppBar(title = { Text(stringResource(id = R.string.main_title))}) },
+        topBar = { TopAppBar(title = { Text(stringResource(id = R.string.main_title)) }) },
         modifier = modifier.fillMaxSize()
     ) { paddingValues ->
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(paddingValues)
-                .padding(top = 16.dp)
-                .fillMaxSize()
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Row {
-                Text(
-                    stringResource(id = R.string.station_of_origin),
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                TextField(value = "", onValueChange = {}, modifier.clickable { onSetOriginStationClicked() }, singleLine = true)
-            }
-
-            Row {
-                Text(
-                    stringResource(id = R.string.station_of_destination),
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                TextField(value = "", onValueChange = {}, modifier.clickable { onSetOriginStationClicked() }, singleLine = true)
-            }
-            Button(
-                onClick = { },
-                enabled = viewModel.buttonEnabled,
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .widthIn(160.dp)
-                    .padding(vertical = 16.dp)
+                    .padding(paddingValues)
+                    .padding(top = 16.dp)
+                    .fillMaxSize()
             ) {
-                Text(stringResource(id = R.string.common_search ))
+                TextWithField(
+                    name = stringResource(id = R.string.station_of_origin),
+                    onPlaceFieldClicked = { onSetOriginStationClicked() }
+                )
+                TextWithField(
+                    name = stringResource(id = R.string.station_of_destination),
+                    onPlaceFieldClicked = { onSetDestinationClicked() }
+                )
+                Button(
+                    onClick = { onSearchButtonClicked() },
+                    enabled = viewModel.buttonEnabled,
+                    modifier = Modifier
+                        .widthIn(240.dp)
+                        .padding(vertical = 16.dp)
+                        .padding(top = 8.dp)
+                ) {
+                    Text(stringResource(id = R.string.common_search))
+                }
             }
         }
     }
+}
+
+@Composable
+fun TextWithField(
+    name: String,
+    modifier: Modifier = Modifier,
+    onPlaceFieldClicked: () -> Unit = {}
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+            .padding(horizontal = 32.dp)
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            name,
+            modifier = Modifier.weight(1f)
+        )
+        TextField(value = "", onValueChange = {},
+            modifier
+                .weight(3f)
+                .clickable { onPlaceFieldClicked() }, singleLine = true
+        )
+    }
+
 }
